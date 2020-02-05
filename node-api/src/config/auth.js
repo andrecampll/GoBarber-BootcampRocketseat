45277b5af33e54b211bb1662ee547123cad0b381
André Victor <andrevictor50@gmail.com>
2020-02-05 09:02:29 -0300

    First back-end commit

---
+++ node-api/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'd37e0e9eed755353c0a36c46cd477ba8',
+  expiresIn: '7d',
+};
---
+++ node-api/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'cb245e94338a15',
+    pass: 'b85ea4d4e1020a',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com',
+  },
+};
